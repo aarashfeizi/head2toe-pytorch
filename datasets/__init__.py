11bd772535b92fcc667a9c0e94f669e324d9b7ae
Aarash Feizi <aarash95@gmail.com>
2022-11-09 14:07:46 -0500

    fix bug

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -6,7 +6,7 @@ DATASETS = {'cifar10': datasets.CIFAR10,
             'svhn': datasets.SVHN}
 
 
-def get_dataset(args, trainsform=None, mode='train'): 
+def get_dataset(args, transform=None, mode='train'): 
     """
         args: 
         mode: train or test
@@ -14,12 +14,12 @@ def get_dataset(args, trainsform=None, mode='train'):
     dataset_name = args.data.dataset
     dataset_root = args.data.dataset_path
     if "cifar" in dataset_name:
-        d = DATASETS[dataset_name](root=dataset_root, train=(mode == 'train'), transform=trainsform)
+        d = DATASETS[dataset_name](root=dataset_root, train=(mode == 'train'), transform=transform)
     elif dataset_name == 'caltech101':
-        d = DATASETS[dataset_name](root=dataset_root, transform=trainsform)
+        d = DATASETS[dataset_name](root=dataset_root, transform=transform)
     elif dataset_name == 'svhn':
         if mode != 'train':
             mode = 'test'
-        d = DATASETS[dataset_name](root=dataset_root, split=mode, transform=trainsform)
+        d = DATASETS[dataset_name](root=dataset_root, split=mode, transform=transform)
 
     return d
