1e3401ff13027be0641f1cab908e78532648676f
Aarash Feizi <aarash95@gmail.com>
2022-11-09 14:01:10 -0500

    read json file better

--- utils.py
+++ utils.py
@@ -25,11 +25,14 @@ def __create_namespace(cfg_dict):
     for k, v in cfg_dict.items():
         if type(v) == dict:
             new_dict[k] = __create_namespace(v)
-        elif v.isnumeric():
-            if '.' in v:
-                new_dict[k] = float(v)
+        if type(v) == str:
+            if v.isnumeric():
+                if '.' in v:
+                    new_dict[k] = float(v)
+                else:
+                    new_dict[k] = int(v)
             else:
-                new_dict[k] = int(v)
+                new_dict[k] = v    
         else:
             new_dict[k] = v
     cfg_ns = argparse.Namespace(**new_dict)
