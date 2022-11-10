3b8cae1cf66c36b87f9bc191c58c6d98b249ba64
Aarash Feizi <aarash95@gmail.com>
2022-11-09 23:00:21 -0500

    only 1000 from cifar

--- datasets/cifar.py
+++ datasets/cifar.py
@@ -4,13 +4,11 @@ from torchvision.datasets import CIFAR10 as tCIFAR10
 class CIFAR100(tCIFAR100):
     def __init__(self,
         root: str,
-        transforms=None,
-        transform=None,
-        target_transform=None):
+        train,
+        transform=None):
         super(CIFAR100, self).__init__(root=root, 
-                                    transforms=transforms, 
-                                    transform=transform, 
-                                    target_transform=target_transform)
+                                    train=train, 
+                                    transform=transform)
 
         self.data = self.data[:1000]
         self.targets = self.targets[:1000] 
@@ -19,13 +17,11 @@ class CIFAR100(tCIFAR100):
 class CIFAR10(tCIFAR10):
     def __init__(self,
         root: str,
-        transforms=None,
-        transform=None,
-        target_transform=None):
+        train,
+        transform=None):
         super(CIFAR10, self).__init__(root=root, 
-                                    transforms=transforms, 
-                                    transform=transform, 
-                                    target_transform=target_transform)
+                                    train=train, 
+                                    transform=transform)
 
         self.data = self.data[:1000]
         self.targets = self.targets[:1000] 
