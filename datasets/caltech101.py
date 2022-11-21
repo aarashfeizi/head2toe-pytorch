ee99a15548263ac9e5a87628781df0b91348d9ae
Aarash Feizi <aarash95@gmail.com>
2022-11-20 23:47:30 -0500

    fix bug

--- datasets/caltech101.py
+++ datasets/caltech101.py
@@ -1,4 +1,5 @@
 from torchvision.datasets import Caltech101 as tCaltech101
+from PIL import Image
 import numpy as np
 import os
 
@@ -24,3 +25,47 @@ class Caltech101(tCaltech101):
         if split == 'train':
             self.index = self.index[:1000]
             self.y = self.y[:1000]
+
+
+    def __getitem__(self, index: int) -> Tuple[Any, Any]:
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (image, target) where the type of target specified by target_type.
+        """
+        import scipy.io
+
+        img = Image.open(
+            os.path.join(
+                self.root,
+                "101_ObjectCategories",
+                self.categories[self.y[index]],
+                f"image_{self.index[index]:04d}.jpg",
+            )
+        ).convert('RGB')
+
+        target = []
+        for t in self.target_type:
+            if t == "category":
+                target.append(self.y[index])
+            elif t == "annotation":
+                data = scipy.io.loadmat(
+                    os.path.join(
+                        self.root,
+                        "Annotations",
+                        self.annotation_categories[self.y[index]],
+                        f"annotation_{self.index[index]:04d}.mat",
+                    )
+                )
+                target.append(data["obj_contour"])
+        target = tuple(target) if len(target) > 1 else target[0]
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target
